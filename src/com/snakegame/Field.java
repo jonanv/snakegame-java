8255930f9d0e4ad0f5224a4b3ed6a3085b587507
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-25 14:19:38 -0500

    Se implemento una interface a la clase Field, se agrego una clase anidad y se dio control a la serpiente

--- src/com/snakegame/Field.java
+++ src/com/snakegame/Field.java
@@ -2,9 +2,10 @@ package com.snakegame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.Random;
 
-public class Field extends JPanel {
+public class Field extends JPanel implements ActionListener {
     int WIDTH = 320;
     int DOT_SIZE = 16;
     int FIELD_SIZE = 320 / 16 * (320 / 16);
@@ -15,6 +16,11 @@ public class Field extends JPanel {
     int[] x = new int[FIELD_SIZE];
     int[] y = new int[FIELD_SIZE];
     int dots;
+    boolean left = false;
+    boolean right = true;
+    boolean up = false;
+    boolean down = false;
+    Timer timer;
 
     void createApple() {
         appleX = new Random().nextInt(WIDTH/DOT_SIZE) * DOT_SIZE;
@@ -24,16 +30,36 @@ public class Field extends JPanel {
     void intGame() {
         dots = 3;
         for (int i = 0; i < dots; i++) {
-            x[i] = 50 - i * DOT_SIZE;
-            y[i] = 50;
+            x[i] = 48 - i * DOT_SIZE;
+            y[i] = 48;
         }
         createApple();
+        timer = new Timer(150, this);
+        timer.start();
     }
 
-    public Field() {
-        setBackground(Color.black);
-        loadImage();
-        intGame();
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+
+    void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if (left) {
+            x[0] = x[0] - DOT_SIZE;
+        }
+        if (right) {
+            x[0] = x[0] + DOT_SIZE;
+        }
+        if (up) {
+            y[0] = y[0] - DOT_SIZE;
+        }
+        if (down) {
+            y[0] = y[0] + DOT_SIZE;
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -44,10 +70,46 @@ public class Field extends JPanel {
         }
     }
 
+    public Field() {
+        setBackground(Color.black);
+        loadImage();
+        intGame();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
+    }
+
     public void loadImage() {
         ImageIcon iia = new ImageIcon("apple.png");
         apple = iia.getImage();
         ImageIcon iid = new ImageIcon("dot.png");
         dot = iid.getImage();
+    }
+
+    class FieldKeyListener extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if (key == KeyEvent.VK_LEFT && !right) {
+                left = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_RIGHT && !left) {
+                right = true;
+                up = false;
+                down = false;
+            }
+            if (key == KeyEvent.VK_UP && !down) {
+                left = false;
+                up = true;
+                right = false;
+            }
+            if (key == KeyEvent.VK_DOWN && !up) {
+                left = false;
+                right = false;
+                down = true;
+            }
+        }
     }
 }
