63cab8e571b16bd2d586192af0c34a80f41d202e
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-25 12:39:15 -0500

    Se agrego la clase Field

---
+++ src/com/snakegame/Field.java
@@ -0,0 +1,20 @@
+package com.snakegame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Field extends JPanel {
+    int WIDTH = 320;
+    int DOT_SIZE = 16;
+    int FIELD_SIZE = 320 / 16 * (320 / 16);
+    Image dot;
+    Image apple;
+
+    public Field() {
+        setBackground(Color.black);
+    }
+
+    public void loadImage() {
+        
+    }
+}
