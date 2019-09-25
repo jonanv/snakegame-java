12feda6ed298266321c03603b2ed956ac772a426
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-09-25 11:38:33 -0500

    Initial commit

---
+++ src/com/snakegame/Main.java
@@ -0,0 +1,8 @@
+package com.snakegame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Haciendo juego de la serpiente en Java con IntelliJ IDEA");
+    }
+}
